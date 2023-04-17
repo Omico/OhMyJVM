c70a4cc59d62356b6be73f62fc006b868ef8ddfe
Omico <me@omico.icu>
2023-04-16 18:54:12 -0700

    Fix initialize mingwX64 target

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
         maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
